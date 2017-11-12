61dc2962414b5b27daf7a86f947b19781e341b05
Maks <maksaturekeshov@gmail.com>
2017-11-12 20:52:25 +0500

    micro changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
   entry: ['babel-polyfill', 'whatwg-fetch', './src/index.js'],
   output: {
@@ -11,6 +13,14 @@ module.exports = {
 	    { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
 	  ]
 	},
+	plugins: [
+    	new UglifyJSPlugin(),
+    	new webpack.DefinePlugin({
+			  'process.env':{
+			    'NODE_ENV': JSON.stringify('production')
+			  }
+			}),
+  ],
 	devServer: {
 	  contentBase: path.join(__dirname, "www"),
 	  port: 3000
